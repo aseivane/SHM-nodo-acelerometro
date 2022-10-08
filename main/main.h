37d998f6126c1bf36b266d7663c53a62b9ff5ee3
Andres Seivane <andyseivane@hotmail.com>
2022-10-07 22:16:12 -0300

    header changes

--- main/main.h
+++ main/main.h
@@ -12,8 +12,6 @@
 #include "configuraciones.h"
 
 
-
-
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -36,20 +34,8 @@
 
 #include "driver/uart.h"
 
-
-#include "sd_card.h"
-#include "acelerometroI2C.h"
 #include "GPIO.h"
 #include "timer_muestreo.h"
-#include "file_server.h"
-
-
-#include "esp_vfs_fat.h"
-#include "driver/sdspi_host.h"
-#include "driver/spi_common.h"
-#include "sdmmc_cmd.h"
-#include "driver/sdmmc_host.h"
-#include "mqtt.h"
 #include "tareas.h"
 
 
@@ -58,52 +44,6 @@ typedef struct mentaje_t {
         bool mensaje_nuevo;
         char mensaje[100];
 } mensaje_t;
-
-
-
-// ESTADOS MUESTREO
-#define ESTADO_ESPERANDO_MENSAJE_DE_INICIO  0
-#define ESTADO_CONFIGURAR_ALARMA_INICIO_A   1
-#define ESTADO_CONFIGURAR_ALARMA_INICIO_B   2
-#define ESTADO_ESPERANDO_INICIO             3
-#define ESTADO_MUESTREANDO                  4
-#define ESTADO_FINALIZANDO_MUESTREO         5
-#define ESTADO_MUESTREANDO_ASYNC            6
-
-
-typedef struct muestreo_t {
-        uint8_t estado_muestreo;
-        int64_t epoch_inicio;  // Epoch (UTC) resolucion en segundos
-        uint32_t int_contador_segundos;  // Contador de la duracion del muestreo
-        uint32_t nro_muestreo;       // Identificador del muestreo en curso
-        uint8_t datos_mpu [CANT_BYTES_LECTURA]; // Lugar donde guardo los datos leidos del mpu
-        uint8_t TABLA0[LONG_TABLAS];
-        uint8_t TABLA1[LONG_TABLAS];
-        uint8_t selec_tabla_escritura;
-        uint8_t selec_tabla_lectura;
-        uint8_t nro_tabla_guardada;
-        uint8_t nro_tabla_enviada;
-        uint32_t nro_muestra_en_seg;
-        uint32_t nro_muestra_total_muestreo;
-        uint32_t nro_archivo;
-        uint32_t duracion_muestreo;
-        bool flag_tomar_muestra;
-        bool flag_muestra_perdida;
-        bool flag_tabla_llena;
-        bool flag_tabla_perdida;
-        bool flag_fin_muestreo;
-        uint32_t cant_muestras_perdidas;  // Contador de muestras perdidas en una tabla.
-        uint32_t cantidad_de_interrupciones_de_muestreo;
-        uint32_t cantidad_de_muestras_leidas;
-}muestreo_t;
-
-
-#define MOUNT_POINT "/sdcard"
-// DMA channel to be used by the SPI peripheral
-#ifndef SPI_DMA_CHAN
-#define SPI_DMA_CHAN    1
-#endif //SPI_DMA_CHAN
-
 
 
 
